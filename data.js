43cfe6f61c5ec43a21abe040061ee2639d2eac00
johnturner4004 <69251742+johnturner4004@users.noreply.github.com>
2021-04-12 20:19:20 -0400

    Create data.js

---
+++ data.js
@@ -0,0 +1,68 @@
+var people = [
+  {
+    name: 'Dev',
+    githubUsername: 'devjanaprime'
+  },
+  {
+    name: 'Dane',
+    githubUsername: 'DoctorHowser'
+  },
+  {
+    name: 'Mary',
+    githubUsername: 'mbmosman'
+  },
+  {
+    name: 'Kris',
+    githubUsername: 'kdszafranski'
+  },
+  {
+    name: 'Edan',
+    githubUsername: 'eschwartz'
+  },
+  {
+    name: 'Casie',
+    githubUsername: 'cassandradanger'
+  },
+  {
+    name: 'Chris',
+    githubUsername: 'christopher-black'
+  },
+  {
+    name: 'John',
+    githubUsername: 'johnturner4004'
+  },
+  {
+    name: 'Johnny',
+    githubUsername: 'Keplej'
+  },
+  {
+    name: 'Robyn',
+    githubUsername: 'rhurd06'
+  },
+  {
+    name: 'Eyram',
+    githubUsername: 'eyramtay'
+  },
+  {
+    name: 'Alex',
+    githubUsername: 'alexlloydwhite'
+  },
+  {
+    name: 'Chloe',
+    githubUsername: 'chloeeverson'
+  },
+  {
+    name: 'Karsten',
+    githubUsername: 'karstenjep'
+  },
+  {
+    name: 'Maneena',
+    githubUsername: 'Manxiong26'
+  },
+  {
+    name: 'Reese',
+    githubUsername: 'Raveryk'
+  },
+  
+
+];
